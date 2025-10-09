0d1ed1c92e94f59d3eab41ef357d187b8dd7a98d
Flyguy83 <benjamin.hendry@student.uts.edu.au>
2025-10-09 02:38:33 +0000

    Gui Node

---
+++ 41068_ws/src/build/41068_ignition_bringup/CMakeFiles/move_bot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move_bot.dir/src/main_move_bot.cpp.o"
+  "CMakeFiles/move_bot.dir/src/main_move_bot.cpp.o.d"
+  "move_bot"
+  "move_bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 41068_ws/src/build/rs1_gui/share/rs1_gui/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/student/RS1-Group5/41068_ws/src/build/rs1_gui"
