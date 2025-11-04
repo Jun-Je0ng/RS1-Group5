176f1d4def03298f609bf5fa7b6bff3320ede341
Flyguy83 <benjamin.hendry@student.uts.edu.au>
2025-11-04 10:04:44 +0000

    gui getting fixed

--- 41068_ws/src/41068_ignition_bringup/src/joy_teleop/setup.py
+++ 41068_ws/src/41068_ignition_bringup/src/joy_teleop/setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'joy_teleop'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='youremail@example.com',
-    description='ROS2 package for Xbox controller teleoperation for Husky robot',
-    license='Apache License 2.0',
+    maintainer='student',
+    maintainer_email='benjamin.hendry@student.uts.edu.au',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'joy_to_twist = joy_teleop.joy_to_twist:main'
         ],
     },
 )
